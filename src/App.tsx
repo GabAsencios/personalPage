589c0902e697846402ddca36683ef662f626542a
GabAsencios <gabriel.asencios98@gmail.com>
2026-05-24 17:12:28 -0400

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -390,16 +390,16 @@ function ProjectGallery() {
         description="
 Reproduced RAFT, combining similarity-based retrieval with an MLP forecaster for long-term time series forecasting. Validated across 10 multivariate benchmarks and a non-stationary e-commerce dataset."
         tech={["Python", "PyTorch", "Pandas"]}
-        projectUrl="https://github.com/GabAsencios/Deep_Learning"
+        projectUrl="https://github.com/GabAsencios/RAFT_Experiment"
       />
 
 
       {/* ANOTHER SMALL CARD */}
       <CardProject 
         title="Database Volleyball Club"
-        description="Created a normalized MySQL relational database (3NF/BCNF) for managing club operations, optimized for data integrity and query performance. Developed a Python Flask web for GUI."
-        tech={["MySQL", "Python", "Flask"]}
-        projectUrl="https://github.com/GabAsencios/VolleyballClubDatabase"
+        description="Created a normalized MySQL relational database (3NF/BCNF) for managing club operations, optimized for data integrity and query performance. Developed a Python streamlit app for GUI."
+        tech={["MySQL", "Python", "Streamlit"]}
+        projectUrl="https://github.com/GabAsencios/MVC-Club-Database"
       />
 
 
@@ -420,7 +420,7 @@ Reproduced RAFT, combining similarity-based retrieval with an MLP forecaster for
         className="md:col-span-2"
         title="My Page – Personal Portfolio Website"
         description="Architected and deployed a full-stack personal portfolio using React and TypeScript, styled with Tailwind CSS for a responsive, modern UI. Built a Node.js backend to manage the contact section's messaging logic and integrated various third-party components for enhanced interactivity. The entire application is hosted on AWS, leveraging cloud infrastructure for high availability and performance."
-        tech={["React", "TypeScript", "Tailwind CSS", "HTML", "Shadcn UI"]}
+        tech={["React", "TypeScript", "NoSQL(Supabase)", "Tailwind CSS", "HTML"]}
         projectUrl="https://github.com/GabAsencios/personalPage"
       />
 
